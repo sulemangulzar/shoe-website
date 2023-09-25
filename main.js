8214f88232ab111fdbd84f922aad90c70005cc8e
sulemangulzar <sulemangulzar07@gmail.com>
2023-09-25 15:05:59 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.remove('hidden')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.add('hidden')
+    })
+}
